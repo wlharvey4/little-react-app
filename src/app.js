e211d06f3bfd69253977e2c9df7142295945014f
Wesley Harvey <pinecone062@gmail.com>
2018-05-03 23:15:16 -0700

    Added React;
    created src/app.js;
    created public/scripts dir;
    sym-linked src/app.js into public/scripts;
    added React code to index.html;
    React app renders.

---
+++ src/app.js
@@ -0,0 +1,13 @@
+/**
+   src/app.js -> public/scripts/app.js
+   ===================================
+   Created: 2018-04-03
+   Version: 0.0.1
+   Author:  wlharvey4
+   -----------------------------------
+ */
+
+ReactDOM.render(
+  React.createElement("h1", null, "A Little React App For Real"),
+  document.getElementById("app")
+);
